90dce1fffebe56e70dca74c50ccdb5f6d71fee78
enrique <enriqueseor@gmail.com>
2024-03-22 19:46:05 +0000

    main class enhancement

--- src/us/plxhack/MEH/Main.java
+++ src/us/plxhack/MEH/Main.java
@@ -11,30 +11,57 @@ import javax.swing.*;
 
 public class Main {
 
-	public static void main(String[] args)
-			throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	public static void main(String[] args) {
+		try {
+			setSystemProperties();
+			setLookAndFeel();
+			JFrame window = createMainWindow();
+			loadPlugins();
+			window.setVisible(true);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(
+					null,
+					"An error occurred: " + e.getMessage(),
+					"Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	private static void setSystemProperties() {
+		System.setProperty("sun.java2d.opengl", "true");
+		System.setProperty("sun.java2d.accthreshold", "0");
+		System.setProperty("awt.useSystemAAFontSettings", "on");
+		System.setProperty("swing.aatext", "true");
+	}
+
+	private static void setLookAndFeel() throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
 		if (System.getProperty("os.name").toLowerCase().contains("nix") ||
 				System.getProperty("os.name").toLowerCase().contains("nux") ||
-				System.getProperty("os.name").toLowerCase().contains("aix"))
+				System.getProperty("os.name").toLowerCase().contains("aix")) {
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+		} else {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+	}
 
-		System.setProperty("sun.java2d.opengl", "True");
-		System.setProperty("sun.java2d.accthreshold", "0");
-		System.setProperty("apple.laf.useScreenMenuBar", "true");
-		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "MEH");
-		System.setProperty("awt.useSystemAAFontSettings","on");
-		System.setProperty("swing.aatext", "true");
+	private static JFrame createMainWindow() {
 		JFrame window = new MainGUI();
-		window.setSize(1024, 736);
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		window.setLocation(dim.width/2-window.getSize().width/2, dim.height/2-window.getSize().height/2);
+		window.setSize(1280, 720);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		window.setLocation(
+				screenSize.width / 2 - window.getSize().width / 2,
+				screenSize.height / 2 - window.getSize().height / 2);
 		window.setTitle("Map Editor of Happiness | No ROM Loaded");
-		window.setMinimumSize(new Dimension(516,338));
+		window.setMinimumSize(new Dimension(640, 360));
 		BankLoader.reset();
-		window.setVisible(true);
+		return window;
+	}
+
+	private static void loadPlugins() {
 		try {
 			PluginManager.loadAllPlugins();
-		} catch (Exception ignored) {}
+		} catch (Exception e) {
+			System.err.println("Failed to load plugins: " + e.getMessage());
+		}
 	}
 }
